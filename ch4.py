7ea15ba8cab4ea0e86f0d874e1b82e5f7ee7d196
Jaemin Ha <woals3527@naver.com>
2022-02-20 22:49:21 +0900

    zz

--- ch4.py
+++ ch4.py
@@ -1,53 +1,59 @@
 import requests
-import replit
+import os
+
 
 def check_url(urls):
-  urls = urls.split(",")
-  domains = [".com", "co.kr", ".net"]
-  for url in urls:
-    url = url.strip()
-    url = url.lower()
-    if not any(domain in url for domain in domains):
-      return print(url + " is not valid.")
-    if "http://" not in url:
-      url = "http://" + url
-    
-    try:
-      response = requests.get(url)
-      status_code = response.status_code
-      if status_code == 200:
-        print(url + " is up!")
-      else:
-        print(url + "is down!")
-    except:
-      print(url + " is error!")
+    urls = urls.split(",")
+    domains = [".com", "co.kr", ".net"]
+    for url in urls:
+        url = url.strip()
+        url = url.lower()
+        if not any(domain in url for domain in domains):
+            return print(url + " is not valid.")
+        if "http://" not in url:
+            url = "http://" + url
+
+        try:
+            response = requests.get(url)
+            status_code = response.status_code
+            if status_code == 200:
+                print(url + " is up!")
+            else:
+                print(url + "is down!")
+        except:
+            print(url + " is error!")
+
 
 def restart_message():
-  while True:
-    print("Do you want to start over? y/n ")
-    answer = input()
-    if answer == "y" or answer == "n":
-      break
-    else:
-      print("That's not a valid answer.")
+    while True:
+        print("Do you want to start over? y/n ")
+        answer = input()
+        if answer == "y" or answer == "n":
+            break
+        else:
+            print("That's not a valid answer.")
 
-  if answer == "y":
-    clear()
-  else:
-    print("k.bye")  
+    if answer == "y":
+        clear()
+    else:
+        print("k.bye")
+
 
 def send_message():
-  urls = input()
-  check_url(urls)
-  restart_message()
+    urls = input()
+    check_url(urls)
+    restart_message()
+
 
 def start():
-  print("Welcome to IsItDown.py")  
-  print("Please write a URL or URLs you want to check. (separated by comma)")
-  send_message()
+    print("Welcome to IsItDown.py")
+    print("Please write a URL or URLs you want to check. (separated by comma)")
+    send_message()
+
 
 def clear():
-  replit.clear()
-  start()
+    os.system('cls')
+    start()
 
-start()  
+
+start()
